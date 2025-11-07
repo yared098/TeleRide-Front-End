5d13410ba98806877695696d3ad949cc792dad1e
yared098 <fdessalew@gmail.com>
2025-11-07 11:47:50 +0300

    add .env file

--- src/Service/authService.js
+++ src/Service/authService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; 
+// const API_URL = "http://localhost:5000/api/auth"; 
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 // ✅ Save user + token helper
 const saveAuthData = (user, token) => {
