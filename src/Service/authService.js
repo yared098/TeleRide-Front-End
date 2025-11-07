eaca2b5fb92baa55905c44853c0de671cc8f679e
yared098 <fdessalew@gmail.com>
2025-11-07 11:54:32 +0300

    add  bug fixed'

--- src/Service/authService.js
+++ src/Service/authService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:5000/api/auth"; 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = `${import.meta.env.VITE_SOCKET_URL}/api/auth`;
 
 
 // ✅ Save user + token helper
